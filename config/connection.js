3c688f3e80b57f85dd7444ef881e206c1e7265e0
JelenaTomic <tomic.elena09@gmail.com>
2022-09-22 22:38:38 -0500

    add

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,15 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'cloud_instance_private_ip',
-    dialect: 'mysql',
-    port: 8889,
-    connectionLimit: 10,
-    socketPath: '/var/run/mysqld/mysqld.sock'
-
-  }
-  
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
+
